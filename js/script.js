888a8facf6b4d964f466bfad210127e9470d21f9
samuelmg9222 <samuelg92@educastur.es>
2024-11-15 09:27:52 +0100

    Cambio en js main

--- js/script.js
+++ js/script.js
@@ -18,7 +18,10 @@ document.addEventListener("DOMContentLoaded", function() {
             valid = false;
         }
 
-
+        if (mensaje.value.trim() === "") {
+            alert("Ingrese un mensaje, por favor");
+            valid = false;
+            }
 
         if (!valid) {
             event.preventDefault();
